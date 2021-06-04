4fc3205a4df6fcd60ed026e1c40f0abd81f9c35e
Osama29 <53341547+Osama29@users.noreply.github.com>
2021-06-04 23:20:12 +0400

    Create Example.cpp

---
+++ Example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+
+    cout << "What is your name?";
+    cin >> name; 
+
+    cout << "Your name is " << name << "." << endl;
+
+    return 0;
+
+}
