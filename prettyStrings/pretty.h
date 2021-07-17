246dd9e16054e52c342455bd2f6baf47e8f50b59
BryanCWH <bcwh3003@gmail.com>
2021-07-17 16:09:12 +0800

    updated

--- prettyStrings/pretty.h
+++ prettyStrings/pretty.h
@@ -1,6 +1,30 @@
-#include <iostream>
-
-using namespace std;
-string title = "=====================================";
-
-string test = "THIS IS A TEST";
+#include <iostream> 
+using namespace std; 
+ 
+ 
+void draw_WELCOME(void) 
+{ 
+    cout << "  \t\t\tw       w   EEEE  LL      CCCC   OOO   M     M   EEEE\n"; 
+    cout << "  \t\t\tw   w   w  E      LL     C      O   O  MM   MM  E    \n"; 
+    cout << "  \t\t\tw   w   w  EEEEE  LL     C      O   O  M M M M  EEEEE\n"; 
+    cout << "  \t\t\t w w w w   E      LL     C      O   O  M  M  M  E    \n";   
+    cout << "  \t\t\t  w   w     EEEE  LLLLLL  CCCC   OOO   M     M   EEEE\n";
+} 
+void draw_TO(void)
+{
+	cout << "\n\t\t\t\t\t   TTTTTTTT   OOO \n"; 
+    cout << "  \t\t\t\t\t   T  TT  T  O   O\n"; 
+    cout << "  \t\t\t\t\t      TT     O   O\n"; 
+    cout << "  \t\t\t\t\t      TT     O   O\n";   
+    cout << "  \t\t\t\t\t      T       OOO \n";
+ 
+}   
+void draw_PARCEL_SYSTEM(void)
+{
+	cout << "\n\t PPP    AAA   RRRR    CCCC   EEEE  LL    \t SSSS  Y   Y   SSSS  TTTTTTTT   EEEE  M     M\n"; 
+    cout << "  \tP   P  A   A  RR  R  C      E      LL    \tS      Y   Y  S      T  TT  T  E      MM   MM\n"; 
+    cout << "  \tPPPP   AAAAA  R RR   C      EEEEE  LL    \tSSSSS   YYY   SSSSS     TT     EEEEE  M M M M\n"; 
+    cout << "  \tP      A   A  R  R   C      E      LL    \t    S    Y        S     TT     E      M  M  M\n";   
+    cout << "  \tP      A   A  R   R   CCCC   EEEE  LLLLL \tSSSS     Y    SSSS      T       EEEE  M     M\n";
+    cout << "\n========================================================================================================";
+}  
